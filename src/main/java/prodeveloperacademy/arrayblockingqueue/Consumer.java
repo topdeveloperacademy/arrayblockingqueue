4195170c8daf8a1b5eba380fe055b976f2ab8e69
Michael Pogrebinsky <smichpog2@gmail.com>
2021-08-10 13:55:48 -0700

    Reformat at code, Update license

--- src/main/java/prodeveloperacademy/arrayblockingqueue/Consumer.java
+++ src/main/java/prodeveloperacademy/arrayblockingqueue/Consumer.java
@@ -1,54 +1,46 @@
 /*
- *
- * MIT License
- *
  * Copyright (c) 2021 Pro Developer Academy <https://www.prodeveloperacademy.com>
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package prodeveloperacademy.arrayblockingqueue;
 
-import java.util.Queue;
 import javafx.application.Platform;
 import javafx.concurrent.Task;
 import javafx.scene.Group;
 import javafx.scene.shape.Shape;
+
+import java.util.Queue;
 
 /**
  * Pro Developer Academy - https://www.prodeveloperacademy.com
  * Authors: Michael P. and Hiram K.
  */
 public class Consumer extends Task<Void> {
-
     private final Group root;
-    private final Queue<Shape> shapes;
+    private final Queue<Shape> shapesQueue;
 
-    public Consumer(Group root, Queue<Shape> shapes) {
+    public Consumer(Group root, Queue<Shape> shapeQueue) {
         this.root = root;
-        this.shapes = shapes;
+        this.shapesQueue = shapeQueue;
     }
 
     @Override
     protected Void call() throws Exception {
         while (true) {
             // Blocks if the queue is empty
-            var shape = shapes.poll();
+            var shape = shapesQueue.poll();
 
             // Send to the UI thread
             Platform.runLater(() -> {
--- src/main/java/prodeveloperacademy/arrayblockingqueue/Producer.java
+++ src/main/java/prodeveloperacademy/arrayblockingqueue/Producer.java
@@ -1,37 +1,28 @@
 /*
- *
- * MIT License
- *
  * Copyright (c) 2021 Pro Developer Academy <https://www.prodeveloperacademy.com>
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package prodeveloperacademy.arrayblockingqueue;
 
-import java.util.Queue;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.ThreadLocalRandom;
 import javafx.concurrent.Task;
-import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
+
+import java.util.Queue;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * Pro Developer Academy - https://www.prodeveloperacademy.com
@@ -49,29 +40,34 @@ public class Producer extends Task<Void> {
         this.height = height;
         this.shapesQueue = shapesQueue;
     }
+
     @Override
-    protected Void call() throws Exception {
-        //Create rectangles endlessly
+    protected Void call() {
+        // Create rectangles endlessly
         while (true) {
-            var random = ThreadLocalRandom.current();
-
-            var xCoordinate = random.nextInt(this.width);
-            var yCoordinate = random.nextInt(this.height);
-            var width = random.nextInt(MAX_SHAPE_WIDTH);
-            var height = random.nextInt(MAX_SHAPE_HEIGHT);
-            var shape = new Rectangle(xCoordinate, yCoordinate, width, height);
-
-            shape.setArcWidth(20);
-            shape.setArcHeight(random.nextInt(20));
-
-            var hue = random.nextDouble() * 360.0;
-            var saturation = random.nextDouble();
-            var brightness = random.nextDouble();
-            var opacity = random.nextDouble();
-            shape.setFill(Color.hsb(hue, saturation, brightness, opacity));
-
+            var shape = createNewShape();
             // Blocks if capacities reached
             shapesQueue.offer(shape);
         }
+    }
+
+    private Shape createNewShape() {
+        var random = ThreadLocalRandom.current();
+
+        var xCoordinate = random.nextInt(this.width);
+        var yCoordinate = random.nextInt(this.height);
+        var width = random.nextInt(MAX_SHAPE_WIDTH);
+        var height = random.nextInt(MAX_SHAPE_HEIGHT);
+        var shape = new Rectangle(xCoordinate, yCoordinate, width, height);
+
+        shape.setArcWidth(20);
+        shape.setArcHeight(random.nextInt(20));
+
+        var hue = random.nextDouble() * 360.0;
+        var saturation = random.nextDouble();
+        var brightness = random.nextDouble();
+        var opacity = random.nextDouble();
+        shape.setFill(Color.hsb(hue, saturation, brightness, opacity));
+        return shape;
     }
 }
